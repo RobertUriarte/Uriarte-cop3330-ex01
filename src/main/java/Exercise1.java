8c827abcb70c49724abf1faf388fff35d7c046c4
RobertUriarte <robbyuriarte2@gmail.com>
2021-06-04 15:45:33 -0400

    Initial commit

---
+++ src/main/java/Exercise1.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+import java.util.Scanner;
+public class Assignment1 {
+    public static void main(String[] args){
+        System.out.print("What is your name ");
+
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        System.out.print("Hello, " + name + ", Nice to meet you!");
+
+
+    }
+}
